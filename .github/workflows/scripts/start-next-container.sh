2b1f3a78c207ea582123965ee888efead7b4eb84
jae woo Jung <awfjol2008@gmail.com>
2025-07-22 23:33:17 +0900

    fix: 도커 컨테이너 실행 전 환경변수 불러오도록 수정

--- .github/workflows/scripts/start-next-container.sh
+++ .github/workflows/scripts/start-next-container.sh
@@ -3,6 +3,8 @@ set -e
 
 cd ~/cicd-practice/.github/workflows/scripts
 
+source .github/workflows/scripts/.deploy-info.env
+
 echo "[INFO] Pulling and starting container: $NEXT_APP"
 
 docker compose pull "$NEXT_APP"
