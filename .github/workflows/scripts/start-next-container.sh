cba9c886416ab40ef4c44e265eb3eeb1a8e24193
jae woo Jung <awfjol2008@gmail.com>
2025-07-22 18:22:23 +0900

    feat: 새로운 컨테이너 Pull 및 기동 스크립트 작성

---
+++ .github/workflows/scripts/start-next-container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cd ./cicd-practice
+source ../.deploy-info.env
+
+echo "[INFO] Pulling and starting container: $NEXT_APP"
+
+docker compose pull "$NEXT_APP"
+docker compose --env-file .env up -d "$NEXT_APP"
